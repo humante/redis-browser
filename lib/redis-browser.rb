1fa0282d31e000e4aca0fd639eacebd1fc384097
Michał Szajbe <michal.szajbe@gmail.com>
2014-07-06 23:12:58 +0200

    Configure with defaults when run as engine

--- lib/redis-browser.rb
+++ lib/redis-browser.rb
@@ -9,11 +9,17 @@ require 'redis-browser/browser'
 require 'redis-browser/web'
 
 module RedisBrowser
+  DEFAULTS = {
+    'connections' => {
+      'default' => 'redis://127.0.0.1:6379/0'
+    }
+  }
+
   def self.configure(opts)
     Web.configure do |config|
-      opts.each do |k, v|
-        config.set k, v
-      end
+      config.set opts
     end
   end
 end
+
+RedisBrowser.configure RedisBrowser::DEFAULTS
