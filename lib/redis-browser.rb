580e994517a2a6f6a58b2bb49210f720ef4ea44b
Ryan Jacobs <ryan.mjacobs@gmail.com>
2014-12-27 16:29:14 -0800

    Default db is 0.

--- lib/redis-browser.rb
+++ lib/redis-browser.rb
@@ -13,7 +13,8 @@ module RedisBrowser
     'connections' => {
       'default' => {
         'host' => '127.0.0.1',
-        'port' => '6379'
+        'port' => '6379',
+        'db' => '0'
       }
     }
   }
